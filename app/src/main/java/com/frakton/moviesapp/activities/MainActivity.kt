8952abaeba8bbb84cd8b2e3558ba65a24d6b4868
Bleona Hasanaj <bleona@frakton.com>
2023-01-10 16:48:54 +0100

    Remove unused import

--- app/src/main/java/com/frakton/moviesapp/activities/MainActivity.kt
+++ app/src/main/java/com/frakton/moviesapp/activities/MainActivity.kt
@@ -2,7 +2,6 @@ package com.frakton.moviesapp.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.frakton.moviesapp.R
 import com.frakton.moviesapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
